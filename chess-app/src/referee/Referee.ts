7cd0cfed621f08f94b8fed3abd017fd20dbbe7b3
Dragos Danilov <dragosdanilov266@gmail.com>
2023-02-28 18:44:09 -0500

    Modify conditionals to prevent illegal pawn jumps over occupied tiles

--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -22,8 +22,12 @@ export default class Referee {
         if(type === PieceType.PAWN) {
             if (team === TeamType.OUR) {
                 if (py === 1) {
-                    if (px === x && (y - py === 1 || y - py === 2)) {
+                    if (px === x && y - py === 1) {
                         if (!this.tileIsOccupied(x, y, boardState)) {
+                            return true;
+                        }
+                    } else if (px === x && y - py === 2) {
+                        if (!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y-1, boardState)) {
                             return true;
                         }
                     }
@@ -36,12 +40,20 @@ export default class Referee {
                 }
             } else {
                 if (py === 6) {
-                    if (px === x && (y - py === -1 || y - py === -2)) {
+                    if (px === x && y - py === -1) {
+                        if (!this.tileIsOccupied(x, y, boardState)) {
                         return true;
+                        }
+                    } else if (px === x && y - py === -2) {
+                        if (!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y+1, boardState)) {
+                            return true;
+                        }
                     }
                 } else {
                     if (px === x && (y - py === -1)) {
+                        if (!this.tileIsOccupied(x, y, boardState)) {
                         return true; 
+                        }
                     }
                 }
             }
