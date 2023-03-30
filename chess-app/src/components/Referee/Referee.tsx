129dc83d12d9709f5718d470403551f5327503e6
Dragos Danilov <dragosdanilov266@gmail.com>
2023-03-30 03:45:12 -0400

    Fix bug where promoted pawn would not preview first move

--- chess-app/src/components/Referee/Referee.tsx
+++ chess-app/src/components/Referee/Referee.tsx
@@ -193,6 +193,7 @@ export default function Referee() {
             return results;
         }, [] as Piece[])
 
+        updatePossibleMoves();
         setPieces(updatedPieces);
 
         modalRef.current?.classList.add("hidden")
